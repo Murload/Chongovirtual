20e63a6ab5f5eefff6413a8567487b9ee93fc541
Murload7 <a.murcia@montechelo.online>
2023-10-23 23:04:40 -0500

    ramas

--- src/main/java/fufiadero/dev/Model/Client.java
+++ src/main/java/fufiadero/dev/Model/Client.java
@@ -4,8 +4,10 @@ public class Client extends Person{
 
     public Client(long id, String name, String lastname, int telephone, String mail) {
         super(id, name, lastname, telephone, mail);
-        
 
-    }
+
+    }   
+
+    
 
 }
--- src/main/java/fufiadero/dev/Model/Prostitute.java
+++ src/main/java/fufiadero/dev/Model/Prostitute.java
@@ -6,6 +6,25 @@ public class Prostitute extends Client {
         super(id, name, lastname, telephone, mail);
     }
     private int price;
+    private String namePros;
+    public int getPrice() {
+        return price;
+    }
+    public void setPrice(int price) {
+        this.price = price;
+    }
+    public String getNamePros() {
+        return namePros;
+    }
+    public void setNamePros(String namePros) {
+        this.namePros = namePros;
+    }
+    public Prostitute(long id, String name, String lastname, int telephone, String mail, int price, String namePros) {
+        super(id, name, lastname, telephone, mail);
+        this.price = price;
+        this.namePros = namePros;
+    }
+
     
     
 }
